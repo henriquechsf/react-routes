2d7553e353db1deea1238fb1ceaf40bc846316ab
Carlos Henrique <ch.ferreir@hotmail.com>
2020-01-13 12:51:30 -0300

    navegação entre paginação com button next e prev

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,6 +6,8 @@ import api from '../../services/api';
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     // funcão de ciclo de vida que é carregada quando o componente é inicializado
@@ -13,15 +15,37 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get("/products");
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        this.setState({ products: response.data.docs })
+        const { docs, ...productInfo } = response.data
+
+        this.setState({ products: docs, productInfo: productInfo, page: page })
+    }
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
     }
 
     render() {
         // destructuring para substituir o this.state.products do map
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             //<h1>Contagem de produtos: { this.state.products.length }</h1>
@@ -34,6 +58,11 @@ export default class Main extends Component {
                         <a href="">Acessar</a>
                     </article>
                 )) }
+
+                <div className="actions">
+                    <button disabled={ page === 1 } onClick={ this.prevPage }>Anterior</button>
+                    <button disabled={ page === productInfo.pages } onClick={ this.nextPage }>Próxima</button>
+                </div>
             </div>
         )
     }
